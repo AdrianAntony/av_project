11e455b02766b4ac03125fad44cec9b0211e9f27
Adrian Antony <adrianantony8@gmail.com>
2021-05-12 18:29:07 +0530

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -7,7 +7,7 @@ $target_path = "uploads/";
 $target_path = $target_path . date("Y-m-d-h-i-s").basename( $_FILES['uploadedfile']['name']); 
 
 move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path);
-mysqli_query($con,"insert into sd_file (file,username) value ('$target_path','$_SESSION[login_user]')");
+mysqli_query($con,"insert into sd_file (file,username,category) value ('$target_path','$_SESSION[login_user]','student file upload')");
 
 
 header("location:note_upload.php");
