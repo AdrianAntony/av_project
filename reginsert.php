e51fa32b97f82d22840e59359a3135482c3b45d5
Adrian Antony <adrianantony8@gmail.com>
2021-06-02 22:51:37 +0530

    Delete reginsert.php

--- reginsert.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("db/connection.php");
-mysqli_query($con,"insert into register (name,username,password,course) value ('$_POST[name]','$_POST[username]','$_POST[password]','$_POST[course]')");
-header("location:register.php?a=1");
-?>
