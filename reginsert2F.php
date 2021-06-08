12c9b6991d44f046efa25ff73ee4e1548011121b
Adrian Antony <adrianantony8@gmail.com>
2021-06-08 19:07:10 +0530

    Delete reginsert2F.php

--- reginsert2F.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("db/connection.php");
-mysqli_query($con,"insert into faculty (name,mobile,email_id,password,course,privilege) value ('$_POST[name]','$_POST[mobile]','$_POST[email_id]','$_POST[password]','$_POST[course]','$_POST[privilege]')");
-header("location:faculty_form.php?a=1");
-?>
